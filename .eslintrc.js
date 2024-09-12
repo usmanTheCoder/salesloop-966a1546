7c0db9deca50f13930f6c9143220a9517108919d
Khawaja Usman <94803671+usmanTheCoder@users.noreply.github.com>
2024-09-12 21:00:01 +0500

    Add/Update .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/display-name': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'no-undef': 'off',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+};
