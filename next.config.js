cb571f014c24dd8a952be218591ecace272b4509
Khawaja Usman <94803671+usmanTheCoder@users.noreply.github.com>
2024-09-12 21:00:09 +0500

    Add/Update next.config.js

---
+++ next.config.js
@@ -0,0 +1,26 @@
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+  images: {
+    domains: ['example.com'], // Add your image domains here
+  },
+};
+
+module.exports = nextConfig;
